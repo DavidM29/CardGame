38ca7d1f5fef26857f4585116499b9be165eb1c0
DavidM29 <Davitm@bit.ge>
2021-12-27 11:45:46 +0400

    Created Classes

---
+++ app/src/main/java/com/example/cardgame/classes/Card.kt
@@ -0,0 +1,5 @@
+package com.example.cardgame.classes
+
+data class Card(val attack: Float, val health: Float, val defence: Float){
+
+}
---
+++ app/src/main/java/com/example/cardgame/classes/Player.kt
@@ -0,0 +1,5 @@
+package com.example.cardgame.classes
+
+data class Player(val email: String, val password: String, val cards: ArrayList<Card>?){
+
+}
