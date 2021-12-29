3933edad746b33a2c8a25e448484fff0470c2463
DavidM29 <Davitm@bit.ge>
2021-12-29 20:56:45 +0400

    Trying To Get Data

--- app/src/main/java/com/example/cardgame/ChooseCardsActivity.kt
+++ app/src/main/java/com/example/cardgame/ChooseCardsActivity.kt
@@ -3,9 +3,17 @@ package com.example.cardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.GridView
 import android.widget.ListView
+import android.widget.Toast
+import androidx.core.view.children
+import com.example.cardgame.classes.Card
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -17,11 +25,42 @@ class ChooseCardsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_cards)
 
-        var auth = FirebaseAuth.getInstance()
+        var cardArray: ArrayList<Card> = ArrayList<Card>()
+        val context = this
+        var list = mutableListOf<String>()
         cardsList = findViewById(R.id.cardsList)
+        var db = FirebaseDatabase.getInstance().getReference("cards")
 
-        var db = FirebaseDatabase.getInstance().getReference("Card")
+        db.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val cards = snapshot.children
 
-        var cardTable = db.child(auth.currentUser?.uid!!).child("Card").child("TestCard").get()//არ მუშაობს!!
+                for (c in cards){
+                    list.add(c.child("Name").value.toString())
+
+                    cardArray.add(
+                        Card(
+                            c.child("Name").value.toString(),
+                            c.child("Attack").value.toString().toFloat(),
+                            c.child("Health").value.toString().toFloat(),
+                            c.child("Defence").value.toString().toFloat(),
+                        )
+                    )
+                }
+
+                var adapter:ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_list_item_1, list)
+                cardsList.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        cardsList.setOnItemClickListener { parent, view, position, id ->
+
+        }
     }
 }
