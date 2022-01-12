779bd6723fe76583e554a0ec2d6ce1b128a89c37
oniani1 <alekooniani2003@gmail.com>
2022-01-12 23:06:03 +0400

    bolo-bolo

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cardgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 import android.widget.TextView
 
--- app/src/main/java/com/example/cardgame/PlayActivity.kt
+++ app/src/main/java/com/example/cardgame/PlayActivity.kt
@@ -3,6 +3,7 @@ package com.example.cardgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -40,6 +41,7 @@ class PlayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_play)
 
         init()
